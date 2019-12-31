61477ec18c64dd54761ced3768ff1136f41d5c96
Christian Kaltepoth <christian@kaltepoth.de>
2019-12-31 11:24:42 +0100

    Added apidocs for 2.1.6 to site

---
+++ apidocs/2.1.6/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"javax.ws.rs"},{"l":"javax.ws.rs.client"},{"l":"javax.ws.rs.container"},{"l":"javax.ws.rs.core"},{"l":"javax.ws.rs.ext"},{"l":"javax.ws.rs.sse"}]
---
+++ apidocs/index.md
@@ -0,0 +1,6 @@
+# Jakarta RESTful Web Services API
+
+## Current Version
+
+* [Jakarta RESTful Web Services 2.1.6](/jaxrs-api/apidocs/2.1.6/)
+

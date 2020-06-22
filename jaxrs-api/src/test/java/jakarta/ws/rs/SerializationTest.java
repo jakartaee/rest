ab67db4af8854d63eaedd8b991f1872db55f7bec
Magnus Brange <brange.m@gmail.com>
2020-06-22 09:02:48 +0200

    Make WebApplicationException serializable
    By marking the Response object as transient, since Response isn't
    serializable
    
    Signed-off-by: Magnus Brange <brange.m@gmail.com>

---
+++ jaxrs-api/src/test/java/jakarta/ws/rs/SerializationTest.java
@@ -0,0 +1,173 @@
+package jakarta.ws.rs;
+
+import jakarta.ws.rs.core.EntityTag;
+import jakarta.ws.rs.core.GenericType;
+import jakarta.ws.rs.core.Link;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.MultivaluedMap;
+import jakarta.ws.rs.core.NewCookie;
+import jakarta.ws.rs.core.Response;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.lang.annotation.Annotation;
+import java.net.URI;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Set;
+import org.junit.Test;
+
+public class SerializationTest {
+
+    class TestResponse extends Response {
+
+        @Override
+        public int getStatus() {
+            return 0;
+        }
+
+        @Override
+        public StatusType getStatusInfo() {
+            return null;
+        }
+
+        @Override
+        public Object getEntity() {
+            return null;
+        }
+
+        @Override
+        public <T> T readEntity(Class<T> entityType) {
+            return null;
+        }
+
+        @Override
+        public <T> T readEntity(GenericType<T> entityType) {
+            return null;
+        }
+
+        @Override
+        public <T> T readEntity(Class<T> entityType, Annotation[] annotations) {
+            return null;
+        }
+
+        @Override
+        public <T> T readEntity(GenericType<T> entityType, Annotation[] annotations) {
+            return null;
+        }
+
+        @Override
+        public boolean hasEntity() {
+            return false;
+        }
+
+        @Override
+        public boolean bufferEntity() {
+            return false;
+        }
+
+        @Override
+        public void close() {
+
+        }
+
+        @Override
+        public MediaType getMediaType() {
+            return null;
+        }
+
+        @Override
+        public Locale getLanguage() {
+            return null;
+        }
+
+        @Override
+        public int getLength() {
+            return 0;
+        }
+
+        @Override
+        public Set<String> getAllowedMethods() {
+            return null;
+        }
+
+        @Override
+        public Map<String, NewCookie> getCookies() {
+            return null;
+        }
+
+        @Override
+        public EntityTag getEntityTag() {
+            return null;
+        }
+
+        @Override
+        public Date getDate() {
+            return null;
+        }
+
+        @Override
+        public Date getLastModified() {
+            return null;
+        }
+
+        @Override
+        public URI getLocation() {
+            return null;
+        }
+
+        @Override
+        public Set<Link> getLinks() {
+            return null;
+        }
+
+        @Override
+        public boolean hasLink(String relation) {
+            return false;
+        }
+
+        @Override
+        public Link getLink(String relation) {
+            return null;
+        }
+
+        @Override
+        public Link.Builder getLinkBuilder(String relation) {
+            return null;
+        }
+
+        @Override
+        public MultivaluedMap<String, Object> getMetadata() {
+            return null;
+        }
+
+        @Override
+        public MultivaluedMap<String, String> getStringHeaders() {
+            return null;
+        }
+
+        @Override
+        public String getHeaderString(String name) {
+            return null;
+        }
+    }
+
+    @Test
+    public void testSerializaWebApplicationException() throws Exception {
+        final WebApplicationException exceptionWithResponse = new WebApplicationException("message", new RuntimeException("cause"), new TestResponse());
+        serialize(exceptionWithResponse);
+
+    }
+
+    private void serialize(WebApplicationException exception) throws IOException {
+        try (
+            ByteArrayOutputStream bos = new ByteArrayOutputStream();
+            ObjectOutputStream oos = new ObjectOutputStream(bos);
+        ) {
+            oos.writeObject(exception);
+            oos.flush();
+            byte[] data = bos.toByteArray();
+        }
+    }
+}

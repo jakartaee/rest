ef72cf12d68cbe04cc6d8f5681f1ec90252917b0
Nicolas NESMON (NicoNes) <nicolas.nesmon@gmail.com>
2021-07-08 13:32:46 -0400

    Add clarification about event consumer exception handling
    Signed-off-by: Nicolas NESMON (NicoNes) <nicolas.nesmon@gmail.com>

--- jaxrs-api/src/main/java/jakarta/ws/rs/sse/SseEventSource.java
+++ jaxrs-api/src/main/java/jakarta/ws/rs/sse/SseEventSource.java
@@ -72,6 +72,11 @@ import jakarta.ws.rs.client.WebTarget;
  * <p>
  * In the case of an error condition response, the <code>Throwable</code> passed to the <code>onError</code> consumer
  * <i>should</i> be a WebApplicationException containing the invalid <code>Response</code> object.
+ * <p>
+ * Note that if, for any of the registered event consumers, invocation of {@link Consumer#accept(Object)
+ * Consumer&lt;InboundSseEvent&gt;#accept(InboundSseEvent)} method throws an exception, this is not an error condition.
+ * Thus <code>onError</code> is not invoked and event processing is not stopped.
+ * User is encouraged to handle exception himself in the event consumer code.
  *
  * @author Marek Potociar
  * @since 2.1

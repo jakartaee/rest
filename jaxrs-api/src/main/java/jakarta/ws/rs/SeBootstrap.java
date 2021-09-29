cb7527a810372c1c75262484b6ec279f548e1723
Gerd Aschemann <eclipse-foundation@aschemann.net>
2021-09-29 19:35:39 +0200

    Fix some unused imports to make checkstyle happy (#978)

--- jaxrs-api/src/main/java/jakarta/ws/rs/SeBootstrap.java
+++ jaxrs-api/src/main/java/jakarta/ws/rs/SeBootstrap.java
@@ -18,7 +18,6 @@ package jakarta.ws.rs;
 
 import java.net.URI;
 import java.util.Optional;
-import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
 import java.util.function.BiFunction;
 import java.util.function.Consumer;

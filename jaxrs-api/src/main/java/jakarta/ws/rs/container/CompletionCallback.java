d7c1b243890a8bd3319576625f995593f6c38f7e
jansupol <15908245+jansupol@users.noreply.github.com>
2021-10-22 20:33:18 +0200

    Clarify exception processing by CompletionCallback and the default ExceptionMapper (#1034)
    * Clarify exception processing of CompletionCallback and the default ExceptionMapper.
    
    Signed-off-by: jansupol <jan.supol@oracle.com>
    
    * Address PR comments
    
    Signed-off-by: jansupol <jan.supol@oracle.com>
    
    * remove outstanding "in"
    
    Signed-off-by: jansupol <jan.supol@oracle.com>

--- jaxrs-api/src/main/java/jakarta/ws/rs/container/CompletionCallback.java
+++ jaxrs-api/src/main/java/jakarta/ws/rs/container/CompletionCallback.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012, 2019 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2012, 2021 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0, which is available at
@@ -19,9 +19,9 @@ package jakarta.ws.rs.container;
 /**
  * A request processing callback that receives request processing completion events.
  * <p>
- * A completion callback is invoked when the whole request processing is over, i.e. once a response for the request has
- * been processed and sent back to the client or in when an unmapped exception or error is being propagated to the
- * container.
+ * A completion callback is always invoked when the whole request processing is over, i.e. once a response for the request has
+ * been processed and sent back to the client (including processing by a custom exception mapper) or when an unmapped
+ * exception or error is being propagated to the default exception mapper.
  * </p>
  *
  * @author Marek Potociar
@@ -29,20 +29,15 @@ package jakarta.ws.rs.container;
  */
 public interface CompletionCallback {
     /**
-     * A completion callback notification method that will be invoked when the request processing is finished, after a
-     * response is processed and is sent back to the client or when an unmapped throwable has been propagated to the hosting
-     * I/O container.
-     * <p>
-     * An unmapped throwable is propagated to the hosting I/O container in case no {@link jakarta.ws.rs.ext.ExceptionMapper
-     * exception mapper} has been found for a throwable indicating a request processing failure. In this case a
-     * non-{@code null} unmapped throwable instance is passed to the method. Note that the throwable instance represents the
-     * actual unmapped exception thrown during the request processing, before it has been wrapped into an I/O
-     * container-specific exception that was used to propagate the throwable to the hosting I/O container.
-     * </p>
+     * An unmapped throwable is propagated to the default exception mapper in case no {@link jakarta.ws.rs.ext.ExceptionMapper
+     * exception mapper} has been found for a request processing failure. In this case a non-{@code null} unmapped throwable
+     * instance is passed to the method. Note that the throwable instance represents the actual unmapped exception thrown during
+     * the request processing before it has been mapped to the response by the default exception mapper.
      *
      * @param throwable is {@code null}, if the request processing has completed with a response that has been sent to the
-     * client. In case the request processing resulted in an unmapped exception or error that has been propagated to the
-     * hosting I/O container, this parameter contains the unmapped exception instance.
+     * client (including processing by a custom exception mapper). In case the request processing resulted in an unmapped
+     * exception or error that has yet to be propagated to the default exception mapper, this parameter contains the unmapped
+     * exception instance.
      */
     public void onComplete(Throwable throwable);
 }

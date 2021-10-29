cce3bfcebef36838c6e96ad08445c90dc0a71cf7
Alwin Joseph <44426046+alwin-joseph@users.noreply.github.com>
2021-10-29 18:27:03 +0530

    TCK Migration : Move a set of jaxrs/ee/rs/ tests from jakartaee-tck (#1031)
    * Initial commit to migrate jaxrs/ee/rs/container,beanparam,constrainedto,delete tests
    
    - change package names to match new folder structure
    
    * rename JAXRS Client java files to *IT before other changes
    
    * fix test setup in ee.rs..container,beanparam,constrainedto,cookieparam,delete
    
    - fix the test setup,
    - comment failing test ee.rs.container.requestcontext.security.getSecurityContextTest
    - add missing files, jar dependency, imports
    - use junit assertTrue

---
+++ jaxrs-tck/src/main/java/jakarta/ws/rs/tck/ee/rs/beanparam/cookie/plain/JAXRSClientIT.java
@@ -0,0 +1,334 @@
+/*
+ * Copyright (c) 2014, 2020 Oracle and/or its affiliates. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0, which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the
+ * Eclipse Public License v. 2.0 are satisfied: GNU General Public License,
+ * version 2 with the GNU Classpath Exception, which is available at
+ * https://www.gnu.org/software/classpath/license.html.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
+ */
+
+package jakarta.ws.rs.tck.ee.rs.beanparam.cookie.plain;
+
+import java.io.InputStream;
+import java.io.IOException;
+import jakarta.ws.rs.tck.lib.util.TestUtil;
+import jakarta.ws.rs.tck.ee.rs.beanparam.BeanParamCommonClient;
+
+import jakarta.ws.rs.core.Response.Status;
+
+import org.jboss.arquillian.junit5.ArquillianExtension;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.StringAsset;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.jboss.shrinkwrap.api.exporter.ZipExporter;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.TestInfo;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterEach;
+
+/*
+ * @class.setup_props: webServerHost;
+ *                     webServerPort;
+ *                     ts_home;
+ * @since 2.0.1                     
+ */
+@ExtendWith(ArquillianExtension.class)
+public class JAXRSClientIT extends BeanParamCommonClient {
+  private static final long serialVersionUID = 201L;
+
+  public JAXRSClientIT() {
+    setup();
+    setContextRoot("/jaxrs_ee_rs_beanparam_cookie_plain_web/resource");
+  }
+
+  @BeforeEach
+  void logStartTest(TestInfo testInfo) {
+    TestUtil.logMsg("STARTING TEST : "+testInfo.getDisplayName());
+  }
+
+  @AfterEach
+  void logFinishTest(TestInfo testInfo) {
+    TestUtil.logMsg("FINISHED TEST : "+testInfo.getDisplayName());
+  }
+
+  @Deployment(testable = false)
+  public static WebArchive createDeployment() throws IOException{
+
+    InputStream inStream = JAXRSClientIT.class.getClassLoader().getResourceAsStream("jakarta/ws/rs/tck/ee/rs/beanparam/cookie/plain/web.xml.template");
+    String webXml = editWebXmlString(inStream);
+
+    WebArchive archive = ShrinkWrap.create(WebArchive.class, "jaxrs_ee_rs_beanparam_cookie_plain_web.war");
+    archive.addClasses(AppConfig.class, Resource.class,
+      jakarta.ws.rs.tck.ee.rs.beanparam.cookie.bean.CookieBeanParamEntity.class,
+      jakarta.ws.rs.tck.ee.rs.beanparam.cookie.bean.InnerCookieBeanParamEntity.class,
+      jakarta.ws.rs.tck.ee.rs.Constants.class,
+      jakarta.ws.rs.tck.ee.rs.ParamEntityPrototype.class,
+      jakarta.ws.rs.tck.ee.rs.ParamEntityWithConstructor.class,
+      jakarta.ws.rs.tck.ee.rs.ParamEntityWithValueOf.class,
+      jakarta.ws.rs.tck.ee.rs.ParamEntityWithFromString.class,
+      jakarta.ws.rs.tck.ee.rs.ParamTest.class,
+      jakarta.ws.rs.tck.ee.rs.JaxrsParamClient.CollectionName.class,
+      jakarta.ws.rs.tck.ee.rs.ParamEntityThrowingWebApplicationException.class,
+      jakarta.ws.rs.tck.ee.rs.ParamEntityThrowingExceptionGivenByName.class,
+      jakarta.ws.rs.tck.ee.rs.RuntimeExceptionMapper.class,
+      jakarta.ws.rs.tck.ee.rs.WebApplicationExceptionMapper.class
+    );
+    archive.setWebXML(new StringAsset(webXml));
+    return archive;
+
+  }
+
+  /* Run test */
+
+  /*
+   * @testName: cookieParamEntityWithConstructorTest
+   * 
+   * @assertion_ids: JAXRS:SPEC:3.5; JAXRS:SPEC:5.2; JAXRS:JAVADOC:6;
+   * JAXRS:JAVADOC:12; JAXRS:JAVADOC:12.1;
+   * 
+   * @test_Strategy: Verify that named CookieParam is handled properly
+   */
+  @Test
+  public void cookieParamEntityWithConstructorTest() throws Fault {
+    super.paramEntityWithConstructorTest();
+  }
+
+  /*
+   * @testName: cookieParamEntityWithValueOfTest
+   * 
+   * @assertion_ids: JAXRS:SPEC:3.5; JAXRS:SPEC:5.3; JAXRS:JAVADOC:6;
+   * JAXRS:JAVADOC:12; JAXRS:JAVADOC:12.1;
+   * 
+   * @test_Strategy: Verify that named CookieParam is handled properly
+   */
+  @Test
+  public void cookieParamEntityWithValueOfTest() throws Fault {
+    super.paramEntityWithValueOfTest();
+  }
+
+  /*
+   * @testName: cookieParamEntityWithFromStringTest
+   * 
+   * @assertion_ids: JAXRS:SPEC:3.5; JAXRS:SPEC:5.3; JAXRS:JAVADOC:6;
+   * JAXRS:JAVADOC:12; JAXRS:JAVADOC:12.1;
+   * 
+   * @test_Strategy: Verify that named CookieParam is handled properly
+   */
+  @Test
+  public void cookieParamEntityWithFromStringTest() throws Fault {
+    super.paramEntityWithFromStringTest();
+  }
+
+  /*
+   * @testName: cookieParamSetEntityWithFromStringTest
+   * 
+   * @assertion_ids: JAXRS:SPEC:3.5; JAXRS:SPEC:5.4; JAXRS:JAVADOC:6;
+   * JAXRS:JAVADOC:12; JAXRS:JAVADOC:12.1;
+   * 
+   * @test_Strategy: Verify that named CookieParam is handled properly
+   */
+  @Test
+  public void cookieParamSetEntityWithFromStringTest() throws Fault {
+    super.paramCollectionEntityWithFromStringTest(CollectionName.SET);
+  }
+
+  /*
+   * @testName: cookieParamSortedSetEntityWithFromStringTest
+   * 
+   * @assertion_ids: JAXRS:SPEC:3.5; JAXRS:SPEC:5.4; JAXRS:JAVADOC:6;
+   * JAXRS:JAVADOC:12; JAXRS:JAVADOC:12.1;
+   * 
+   * @test_Strategy: Verify that named CookieParam is handled properly
+   */
+  @Test
+  public void cookieParamSortedSetEntityWithFromStringTest() throws Fault {
+    super.paramCollectionEntityWithFromStringTest(CollectionName.SORTED_SET);
+  }
+
+  /*
+   * @testName: cookieParamListEntityWithFromStringTest
+   * 
+   * @assertion_ids: JAXRS:SPEC:3.5; JAXRS:SPEC:5.4; JAXRS:JAVADOC:6;
+   * JAXRS:JAVADOC:12; JAXRS:JAVADOC:12.1;
+   * 
+   * @test_Strategy: Verify that named CookieParam is handled properly
+   */
+  @Test
+  public void cookieParamListEntityWithFromStringTest() throws Fault {
+    super.paramCollectionEntityWithFromStringTest(CollectionName.LIST);
+  }
+
+  /*
+   * @testName: cookieFieldParamEntityWithConstructorTest
+   * 
+   * @assertion_ids: JAXRS:SPEC:3.5; JAXRS:SPEC:5.2; JAXRS:JAVADOC:6;
+   * 
+   * @test_Strategy: Verify that named CookieParam is handled properly
+   */
+  @Test
+  public void cookieFieldParamEntityWithConstructorTest() throws Fault {
+    super.fieldEntityWithConstructorTest();
+  }
+
+  /*
+   * @testName: cookieFieldParamEntityWithValueOfTest
+   * 
+   * @assertion_ids: JAXRS:SPEC:3.5; JAXRS:SPEC:5.3; JAXRS:JAVADOC:6;
+   * 
+   * @test_Strategy: Verify that named CookieParam is handled properly
+   */
+  @Test
+  public void cookieFieldParamEntityWithValueOfTest() throws Fault {
+    super.fieldEntityWithValueOfTest();
+  }
+
+  /*
+   * @testName: cookieFieldParamEntityWithFromStringTest
+   * 
+   * @assertion_ids: JAXRS:SPEC:3.5; JAXRS:SPEC:5.3; JAXRS:JAVADOC:6;
+   * 
+   * @test_Strategy: Verify that named CookieParam is handled properly
+   */
+  @Test
+  public void cookieFieldParamEntityWithFromStringTest() throws Fault {
+    super.fieldEntityWithFromStringTest();
+  }
+
+  /*
+   * @testName: cookieFieldParamSetEntityWithFromStringTest
+   * 
+   * @assertion_ids: JAXRS:SPEC:3.5; JAXRS:SPEC:5.4; JAXRS:JAVADOC:6;
+   * 
+   * @test_Strategy: Verify that named CookieParam is handled properly
+   */
+  @Test
+  public void cookieFieldParamSetEntityWithFromStringTest() throws Fault {
+    super.fieldCollectionEntityWithFromStringTest(CollectionName.SET);
+  }
+
+  /*
+   * @testName: cookieFieldParamSortedSetEntityWithFromStringTest
+   * 
+   * @assertion_ids: JAXRS:SPEC:3.5; JAXRS:SPEC:5.4; JAXRS:JAVADOC:6;
+   * 
+   * @test_Strategy: Verify that named CookieParam is handled properly
+   */
+  @Test
+  public void cookieFieldParamSortedSetEntityWithFromStringTest() throws Fault {
+    super.fieldCollectionEntityWithFromStringTest(CollectionName.SORTED_SET);
+  }
+
+  /*
+   * @testName: cookieFieldParamListEntityWithFromStringTest
+   * 
+   * @assertion_ids: JAXRS:SPEC:3.5; JAXRS:SPEC:5.4; JAXRS:JAVADOC:6;
+   * 
+   * @test_Strategy: Verify that named CookieParam is handled properly
+   */
+  @Test
+  public void cookieFieldParamListEntityWithFromStringTest() throws Fault {
+    super.fieldCollectionEntityWithFromStringTest(CollectionName.LIST);
+  }
+
+  /*
+   * @testName: cookieParamThrowingWebApplicationExceptionTest
+   * 
+   * @assertion_ids: JAXRS:SPEC:12.3;
+   * 
+   * @test_Strategy: Exceptions thrown during construction of parameter values
+   * are treated the same as exceptions thrown during construction of field or
+   * bean property values, see Section 3.2.
+   */
+  @Test
+  public void cookieParamThrowingWebApplicationExceptionTest() throws Fault {
+    super.paramThrowingWebApplicationExceptionTest();
+    super.paramThrowingWebApplicationExceptionTest();
+  }
+
+  /*
+   * @testName: cookieFieldThrowingWebApplicationExceptionTest
+   * 
+   * @assertion_ids: JAXRS:SPEC:3.1; JAXRS:SPEC:8;
+   * 
+   * @test_Strategy: A WebApplicationException thrown during construction of
+   * field or property values using 2 or 3 above is processed directly as
+   * described in section 3.3.4.
+   */
+  @Test
+  public void cookieFieldThrowingWebApplicationExceptionTest() throws Fault {
+    super.fieldThrowingWebApplicationExceptionTest();
+    super.fieldThrowingWebApplicationExceptionTest();
+  }
+
+  /*
+   * @testName: cookieParamThrowingIllegalArgumentExceptionTest
+   * 
+   * @assertion_ids: JAXRS:SPEC:12.3;
+   * 
+   * @test_Strategy: Exceptions thrown during construction of parameter values
+   * are treated the same as exceptions thrown during construction of field or
+   * bean property values, see section 3.2.
+   */
+  @Test
+  public void cookieParamThrowingIllegalArgumentExceptionTest() throws Fault {
+    super.paramThrowingIllegalArgumentExceptionTest();
+    super.paramThrowingIllegalArgumentExceptionTest();
+  }
+
+  /*
+   * @testName: cookieFieldThrowingIllegalArgumentExceptionTest
+   * 
+   * @assertion_ids: JAXRS:SPEC:9; JAXRS:SPEC:9.2; JAXRS:SPEC:10;
+   * 
+   * @test_Strategy: Other exceptions thrown during construction of field or
+   * property values using 2 or 3 above are treated as client errors:
+   *
+   * if the field or property is annotated with @FormParam or @CookieParam then
+   * an implementation MUST generate a WebApplicationException that wraps the
+   * thrown exception with a client error response (400 status) and no entity.
+   */
+  @Test
+  public void cookieFieldThrowingIllegalArgumentExceptionTest() throws Fault {
+    super.fieldThrowingIllegalArgumentExceptionTest();
+    super.fieldThrowingIllegalArgumentExceptionTest();
+  }
+
+  @Override
+  protected String buildRequest(String param) {
+    return buildRequest(Request.POST, fieldBeanParam);
+  }
+
+  @Override
+  protected void invoke() throws Fault {
+    setProperty(Property.CONTENT, "Anything");
+    super.invoke();
+  }
+
+  @Override
+  protected void paramEntity(String request) throws Fault {
+    createAndCheckCookie(request, "Set");
+    super.paramEntity(request);
+  }
+
+  @Override
+  protected void paramEntityThrowingAfterRequestSet(String request)
+      throws Fault {
+    createAndCheckCookie(request, "Set");
+    // this is for illegal argument exception tests only
+    if (request.contains(IllegalArgumentException.class.getSimpleName()))
+      setProperty(Property.UNORDERED_SEARCH_STRING, Status.BAD_REQUEST.name());
+    super.paramEntityThrowingAfterRequestSet(request);
+  }
+}
---
+++ jaxrs-tck/src/main/java/jakarta/ws/rs/tck/ee/rs/container/requestcontext/security/JAXRSClientIT.java
@@ -0,0 +1,132 @@
+/*
+ * Copyright (c) 2012, 2020 Oracle and/or its affiliates. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0, which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the
+ * Eclipse Public License v. 2.0 are satisfied: GNU General Public License,
+ * version 2 with the GNU Classpath Exception, which is available at
+ * https://www.gnu.org/software/classpath/license.html.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
+ */
+
+package jakarta.ws.rs.tck.ee.rs.container.requestcontext.security;
+
+import java.io.InputStream;
+import java.io.IOException;
+import java.util.Properties;
+import jakarta.ws.rs.tck.lib.util.TestUtil;
+
+import jakarta.ws.rs.tck.common.client.JaxrsCommonClient;
+
+import jakarta.ws.rs.core.Response.Status;
+
+import org.jboss.arquillian.junit5.ArquillianExtension;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.StringAsset;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.jboss.shrinkwrap.api.exporter.ZipExporter;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.TestInfo;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterEach;
+
+/*
+ * @class.setup_props: webServerHost;
+ *                     webServerPort;
+ *                     ts_home;
+ *                     user;
+ *                     password;                     
+ */
+@ExtendWith(ArquillianExtension.class)
+public class JAXRSClientIT extends JaxrsCommonClient {
+
+  private static final long serialVersionUID = -3020219607348263568L;
+
+  protected String user;
+
+  protected String password;
+
+  public JAXRSClientIT() {
+    usersetup();
+    setContextRoot("/jaxrs_ee_rs_container_requestcontext_security_web/resource");
+  }
+
+  @BeforeEach
+  void logStartTest(TestInfo testInfo) {
+    TestUtil.logMsg("STARTING TEST : "+testInfo.getDisplayName());
+  }
+
+  @AfterEach
+  void logFinishTest(TestInfo testInfo) {
+    TestUtil.logMsg("FINISHED TEST : "+testInfo.getDisplayName());
+  }
+
+  @Deployment(testable = false)
+  public static WebArchive createDeployment() throws IOException {
+
+    InputStream inStream = JAXRSClientIT.class.getClassLoader().getResourceAsStream("jakarta/ws/rs/tck/ee/rs/container/requestcontext/security/web.xml.template");
+    String webXml = editWebXmlString(inStream);
+
+    WebArchive archive = ShrinkWrap.create(WebArchive.class, "jaxrs_ee_rs_container_requestcontext_security_web.war");
+    archive.addClasses(TSAppConfig.class, Resource.class, RequestFilter.class);
+    archive.addAsResource("jakarta/ws/rs/tck/ee/rs/container/requestcontext/security/jaxrs_ee_rs_container_requestcontext_security_web.war.sun-web.xml");
+    archive.setWebXML(new StringAsset(webXml));
+    return archive;
+
+  }
+
+  public void usersetup() {
+    user = System.getProperty("user");
+    password = System.getProperty("password");
+    assertTrue(!isNullOrEmpty(user), "user not set");
+    assertTrue(!isNullOrEmpty(password),
+        "password not set");
+    super.setup();
+  }
+
+  /*
+   * @testName: getSecurityContextTest
+   * 
+   * @assertion_ids: JAXRS:JAVADOC:664;
+   * 
+   * @test_Strategy: Get the injectable security context information for the
+   * current request, the user is authenticated.
+   */
+  //@Test
+  public void getSecurityContextTest() throws Fault {
+    setProperty(Property.BASIC_AUTH_USER, user);
+    setProperty(Property.BASIC_AUTH_PASSWD, password);
+    setProperty(Property.SEARCH_STRING, user);
+    String request = buildRequest(Request.POST, "");
+    setProperty(Property.REQUEST, request);
+    invoke();
+  }
+
+  /*
+   * @testName: noSecurityTest
+   * 
+   * @assertion_ids: JAXRS:JAVADOC:664;
+   * 
+   * @test_Strategy: Make sure the authorization is needed
+   */
+  @Test
+  public void noSecurityTest() throws Fault {
+    String request = buildRequest(Request.POST, "");
+    setProperty(Property.REQUEST, request);
+    setProperty(Property.STATUS_CODE, getStatusCode(Status.UNAUTHORIZED));
+    invoke();
+  }
+
+  // ////////////////////////////////////////////////////////////////////////////
+
+}

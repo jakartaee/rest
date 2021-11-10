7997e9a5536f65db695a2dd2e35c0bf4ae5544cb
jansupol <15908245+jansupol@users.noreply.github.com>
2021-11-10 16:16:40 +0100

    Fix NPE for NewCookie#equals when simesite is null (#1056)
    Signed-off-by: jansupol <jan.supol@oracle.com>

--- jaxrs-api/src/main/java/jakarta/ws/rs/core/NewCookie.java
+++ jaxrs-api/src/main/java/jakarta/ws/rs/core/NewCookie.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010, 2020 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2010, 2021 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0, which is available at
@@ -429,7 +429,7 @@ public class NewCookie extends Cookie {
         hash = 59 + hash + (this.expiry != null ? this.expiry.hashCode() : 0);
         hash = 59 * hash + (this.secure ? 1 : 0);
         hash = 59 * hash + (this.httpOnly ? 1 : 0);
-        hash = 59 * hash + this.sameSite.ordinal();
+        hash = 59 * hash + (this.sameSite != null ? this.sameSite.ordinal() : 0);
         return hash;
     }
 

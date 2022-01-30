876223d29d5d2e4339ef9478075525b3fdd91bc1
Markus KARG <markus@headcrashing.eu>
2022-01-30 14:11:27 +0000

    Moved contextprovider package into spec package of TCK

--- jaxrs-tck/src/main/java/jakarta/ws/rs/tck/spec/contextprovider/JsonbContextProviderIT.java
+++ jaxrs-tck/src/main/java/jakarta/ws/rs/tck/spec/contextprovider/JsonbContextProviderIT.java
@@ -14,7 +14,7 @@
  * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
  */
 
-package jakarta.ws.rs.tck.contextprovider;
+package jakarta.ws.rs.tck.spec.contextprovider;
 
 import static java.util.concurrent.TimeUnit.HOURS;
 

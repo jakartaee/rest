36c4c49f61b0c5e5d3011c7cc9d895f1afb24f2e
Markus KARG <markus@headcrashing.eu>
2022-02-10 08:12:18 +0000

    [1082] Rename TCK packages from jakarta.ws.rs.tck to ee.jakarta.tck.ws.rs. (#1082)

--- jaxrs-tck/src/main/java/ee/jakarta/tck/ws/rs/spec/contextprovider/JsonbContextProviderIT.java
+++ jaxrs-tck/src/main/java/ee/jakarta/tck/ws/rs/spec/contextprovider/JsonbContextProviderIT.java
@@ -14,7 +14,7 @@
  * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
  */
 
-package jakarta.ws.rs.tck.spec.contextprovider;
+package ee.jakarta.tck.ws.rs.spec.contextprovider;
 
 import static java.util.concurrent.TimeUnit.HOURS;
 

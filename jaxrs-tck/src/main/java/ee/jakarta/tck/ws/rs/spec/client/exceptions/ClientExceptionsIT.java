93df01a7f900ef60ca06e9d4adb78ea9c12fef4d
jim-krueger <jckofbyron@gmail.com>
2022-09-13 16:26:12 -0500

    Remove final from Jakarta Rest Application subclasses containing Bean Defining Annotations.  CDI rejects final classes.

--- jaxrs-tck/src/main/java/ee/jakarta/tck/ws/rs/spec/client/exceptions/ClientExceptionsIT.java
+++ jaxrs-tck/src/main/java/ee/jakarta/tck/ws/rs/spec/client/exceptions/ClientExceptionsIT.java
@@ -197,7 +197,7 @@ public class ClientExceptionsIT {
     }
 
     @ApplicationPath("")
-    public static final class StatusApplication extends Application {
+    public static class StatusApplication extends Application {
 
         @Override
         public Set<Class<?>> getClasses() {

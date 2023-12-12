95d7290e90e854f7c6c394be34fc347bfe88f70e
Jorge Bescos Gascon <jorge.bescos.gascon@oracle.com>
2023-12-12 16:49:25 +0100

    TCK Challenge: ee.jakarta.tck.ws.rs.ee.rs.container.responsecontext.JAXRSClientIT#setStatusInfoTest and #setStatusTest #1199 (#1201)
    Signed-off-by: Jorge Bescos Gascon <jorge.bescos.gascon@oracle.com>

--- jaxrs-tck/src/main/java/ee/jakarta/tck/ws/rs/ee/rs/container/responsecontext/ResponseFilter.java
+++ jaxrs-tck/src/main/java/ee/jakarta/tck/ws/rs/ee/rs/container/responsecontext/ResponseFilter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012, 2020 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2012, 2023 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0, which is available at
@@ -362,6 +362,7 @@ public class ResponseFilter extends TemplateFilter {
   private void resetStatusEntity(int status) {
     switch (status) {
     case 204:
+    case 304:
     case 205:
       responseContext.setEntity(null);
       break;

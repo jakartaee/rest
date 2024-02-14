0131ae0956b8abaaa2db5fa2d99e9e20ccb7e639
Alwin Joseph <44426046+alwin-joseph@users.noreply.github.com>
2024-02-14 19:02:45 +0530

    Version changes for TCK service release 3.1.5 (#1218)
    * version changes for TCK service release 3.1.5
    
    * correct copyright year

--- jaxrs-tck-docs/tckbundle.sh
+++ jaxrs-tck-docs/tckbundle.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -xe
 
-# Copyright (c) 2023 Oracle and/or its affiliates. All rights reserved.
+# Copyright (c) 2023, 2024 Oracle and/or its affiliates. All rights reserved.
 #
 # This program and the accompanying materials are made available under the
 # terms of the Eclipse Public License v. 2.0, which is available at
@@ -23,7 +23,7 @@ cd $WORKSPACE
 export VERSION="$2"
 
 if [ -z "$VERSION" ]; then
-  export VERSION="3.1.4"
+  export VERSION="3.1.5"
 fi
 
 if [[ "$1" == "epl" || "$1" == "EPL" ]]; then

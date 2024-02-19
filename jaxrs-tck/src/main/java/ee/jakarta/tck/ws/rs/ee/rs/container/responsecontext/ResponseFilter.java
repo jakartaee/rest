4397943ebcfb28ecc9202f9f39e8069d22fab63c
jim-krueger <jckofbyron@gmail.com>
2024-02-19 14:26:40 -0600

    Add 4.0 change from PR 1164

--- jaxrs-tck/src/main/java/ee/jakarta/tck/ws/rs/ee/rs/container/responsecontext/ResponseFilter.java
+++ jaxrs-tck/src/main/java/ee/jakarta/tck/ws/rs/ee/rs/container/responsecontext/ResponseFilter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012, 2023 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2012, 2024 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0, which is available at
@@ -315,6 +315,11 @@ public class ResponseFilter extends TemplateFilter {
       public synchronized void write(byte[] b, int off, int len)
           throws IOException {
         write(b);
+      }
+
+      @Override
+      public void close() throws IOException {
+        stream.close();
       }
 
       public final byte[] intToByteArray(int value) {

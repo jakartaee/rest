769d0829d697ba8698394d9973b3fdc2fa573e84
Neena Jacob <neena.jacob@ibm.com>
2026-07-24 13:37:34 +0530

    Keeping on the closed event

--- jaxrs-api/src/main/java/jakarta/ws/rs/client/ClientListener.java
+++ jaxrs-api/src/main/java/jakarta/ws/rs/client/ClientListener.java
@@ -17,30 +17,13 @@
 package jakarta.ws.rs.client;
 
 /**
- * A listener interface for receiving client lifecycle events.
+ * A listener interface for receiving client close notifications.
  * <p>
  * Implementations of this interface can be registered with a {@link ClientBuilder}
- * to receive notifications about client lifecycle events such as connection,
- * disconnection, and failures.
- * </p>
- * <p>
- * All methods in this interface have default implementations that do nothing,
- * allowing implementations to override only the events they are interested in.
+ * to receive notification when a {@link Client} is closed.
  * </p>
  */
 public interface ClientListener {
-
-    /**
-     * Called when the client successfully establishes a connection.
-     * <p>
-     * This method is invoked after the client has been created and is ready
-     * to process requests.
-     * </p>
-     *
-     * @param client the client that connected
-     */
-    default void connected(Client client) {
-    }
 
     /**
      * Called when the client is closed.
@@ -53,33 +36,5 @@ public interface ClientListener {
      * @param client the client that was closed
      */
     default void closed(Client client) {
-    }
-
-    /**
-     * Called when a connection failure occurs.
-     * <p>
-     * This method is invoked when the client encounters a connection error
-     * that prevents it from communicating with the server. This could be
-     * due to network issues, server unavailability, or other connection
-     * problems.
-     * </p>
-     *
-     * @param client the client that experienced the failure
-     * @param cause  the exception that caused the failure
-     */
-    default void connectionFailed(Client client, Throwable cause) {
-    }
-
-    /**
-     * Called when the client attempts to reconnect after a connection failure.
-     * <p>
-     * This method is invoked when the client detects a connection failure
-     * and is attempting to re-establish the connection. This can be used
-     * to notify users or log reconnection attempts.
-     * </p>
-     *
-     * @param client the client that is reconnecting
-     */
-    default void reconnecting(Client client) {
     }
 }
